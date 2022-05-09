1a15ff214f4323299db85787e4d39ba3318dc7a0
Svetlana <90257465+ssolomvi@users.noreply.github.com>
2022-05-09 11:04:48 +0300

    Update tree.c

--- 23/13/tree.c
+++ 23/13/tree.c
@@ -1,162 +1,228 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-
 #include "tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Forest = list of trees
-// Tree = node's value + children forest + pointer to first child (for convenience)
-
-typedef struct tagForest *Forest;
-
-struct tagTree {
-    TreeItem value;
-    Forest children;    // Double-linked list of child trees (== Forest)
-    Forest first_child; // Only for convenience!
-};
-
-struct tagForest {
-    Tree node;
-    Forest next;
-    Forest prev;
-};
-
-
-Forest forest_create(Tree tree);
-Forest forest_append_tree(Forest forest, Tree tree);
-
-
-Tree tree_create(TreeItem rootValue)
+void find_ways_to(tree context, T value, way_with_target** target_ways, int* target_ways_count)
 {
-    Tree result = (Tree) malloc(sizeof(*result));
+    way constructed_way;
+    constructed_way.chain = NULL;
+    constructed_way.chain_length = 0;
 
-    result->value = rootValue;
-    result->children = NULL;
-    result->first_child = NULL;
+    *target_ways = NULL;
+    *target_ways_count = 0;
 
-    return result;
+    inner_find_ways_to(context.root, value, &constructed_way, target_ways, target_ways_count);
+
+    free(constructed_way.chain);
 }
 
-Forest forest_create(Tree tree)
+void inner_find_ways_to(tree_item* subtree_root, T value, way* constructed_way,
+                        way_with_target** target_ways, int* target_ways_count)
 {
-    Forest result = (Forest) malloc(sizeof(*result));
+    int i;
 
-    result->node = tree;
-    result->next = NULL;
-    result->prev = NULL;
-
-    return result;
-}
-
-Tree *tree_add(Tree parent, TreeItem value)
-{
-    Tree child = tree_create(value);
-
-    if (parent->children == NULL) {
-        parent->children = forest_create(child);
-        parent->first_child = parent->children;
-    } else {
-        parent->children = forest_append_tree(parent->children, child);
-    }
-
-    return &(parent->children->node);
-}
-
-Forest forest_append_tree(Forest forest, Tree tree)
-{
-    Forest result = forest_create(tree);
-
-    result->prev = forest;
-    forest->next = result;
-
-    return result;
-}
-
-void tree_print_children(Tree tree, int depth)
-{
-    if (tree == NULL) {
+    if (subtree_root == NULL)
+    {
         return;
     }
 
-    if (tree->children != NULL) {
-        for (int i = 0; i < depth; ++i) {
-            printf("      ");
+    if (subtree_root->data == value)
+    {
+        *target_ways = (way_with_target*)realloc(*target_ways,
+                                                 ++*target_ways_count * sizeof(way_with_target));
+
+        (*target_ways)[(*target_ways_count) - 1].target = subtree_root;
+        (*target_ways)[(*target_ways_count) - 1].way_to_target.chain_length = constructed_way->chain_length;
+        (*target_ways)[(*target_ways_count) - 1].way_to_target.chain = (way_item*)malloc(
+                constructed_way->chain_length * sizeof(way_item));
+        memcpy((*target_ways)[(*target_ways_count) - 1].way_to_target.chain, constructed_way->chain,
+               constructed_way->chain_length * sizeof(way_item));
+    }
+
+    constructed_way->chain = (way_item*)realloc(constructed_way->chain,
+                                                ++constructed_way->chain_length * sizeof(way_item));
+    constructed_way->chain[constructed_way->chain_length - 1].value = subtree_root->data;
+
+    for (i = 0; i < subtree_root->subtrees_count; i++)
+    {
+        constructed_way->chain[constructed_way->chain_length - 1].index_from_parent_item = i;
+
+        inner_find_ways_to(subtree_root->subtrees + i, value, constructed_way,
+                           target_ways, target_ways_count);
+    }
+
+    constructed_way->chain = (way_item*)realloc(constructed_way->chain,
+                                                --constructed_way->chain_length * sizeof(way_item));
+}
+
+void find_parent(tree context, tree_item* child, tree_item** target_parent, int* child_index)
+{
+    *target_parent = NULL;
+
+    find_parent_inner(context.root, child, target_parent, child_index);
+}
+
+void find_parent_inner(tree_item* subtree_root, tree_item* child, tree_item** target_parent, int* child_index)
+{
+    int i;
+
+    if (subtree_root == NULL)
+    {
+        return;
+    }
+
+    for (i = 0; i < subtree_root->subtrees_count; i++)
+    {
+        if (subtree_root->subtrees + i == child)
+        {
+            *target_parent = subtree_root;
+            *child_index = i;
+            return;
         }
-        depth == 0 ? printf("%c|\n", tree->value) : printf("|---%c\n", tree->value);
-        for (Forest child = tree->first_child; child != NULL; child = child->next) {
-            tree_print_children(child->node, depth + 1);
+
+        find_parent_inner(subtree_root->subtrees + i, child, target_parent, child_index);
+
+        if (*target_parent != NULL)
+        {
+            return;
         }
-    } else {
-        for (int i = 0; i < depth; ++i) {
-            printf("      ");
-        }
-        printf("|---%c|\n", tree->value);
     }
 }
 
-void tree_print(Tree tree)
+void free_item_and_null(tree_item** to_free)
 {
-    if (tree != NULL && tree->children != NULL) {
-        tree_print_children(tree, 0);
-    } else {
-        printf("Empty tree\n");
+    if (*to_free == NULL)
+    {
+        // TODO: validation
+        return;
+    }
+
+    free((*to_free)->subtrees);
+    free((*to_free));
+    *to_free = NULL;
+}
+
+void init_tree(tree* target_tree)
+{
+    target_tree->root = NULL;
+}
+
+void insert_item(tree* target_tree, tree_item* parent_for_item_to_insert, T value)
+{
+    if (target_tree->root == NULL)
+    {
+        target_tree->root = (tree_item*)malloc(sizeof(tree_item));
+        target_tree->root->data = value;
+        target_tree->root->subtrees_count = 0;
+        target_tree->root->subtrees = NULL;
+
+        return;
+    }
+
+    parent_for_item_to_insert->subtrees = (tree_item*)realloc(parent_for_item_to_insert->subtrees,
+                                                              ++parent_for_item_to_insert->subtrees_count * sizeof(tree_item));
+
+    parent_for_item_to_insert->subtrees[parent_for_item_to_insert->subtrees_count - 1].subtrees = NULL;
+    parent_for_item_to_insert->subtrees[parent_for_item_to_insert->subtrees_count - 1].subtrees_count = 0;
+    parent_for_item_to_insert->subtrees[parent_for_item_to_insert->subtrees_count - 1].data = value;
+}
+
+void remove_item(tree* target_tree, tree_item* to_remove)
+{
+    tree_item* to_remove_parent;
+    int to_remove_index_in_parent_context;
+
+    if (target_tree == NULL)
+    {
+        return;
+    }
+
+    if (to_remove == NULL)
+    {
+        return;
+    }
+
+    if (target_tree->root == NULL)
+    {
+        return;
+    }
+
+    if (target_tree->root == to_remove)
+    {
+        free_item_and_null(&target_tree->root);
+        return;
+    }
+
+    find_parent(*target_tree, to_remove, &to_remove_parent, &to_remove_index_in_parent_context);
+
+    if (to_remove_parent == NULL)
+    {
+        printf("The element to remove does not exist. Try again...\n");
+        // TODO: val // АШИПКА ПЭРЭНТ НЕ НАЙДЕН! ?? может выведем в консоль? ^_^ А почему мы не инициализируем to_remove_parent?
+        return;
+    }
+
+    memcpy(to_remove_parent->subtrees + to_remove_index_in_parent_context,
+           to_remove_parent->subtrees + to_remove_index_in_parent_context + 1,
+           --to_remove_parent->subtrees_count * sizeof(tree_item));
+
+    to_remove_parent->subtrees = (tree_item*)realloc(to_remove_parent->subtrees,
+                                                     to_remove_parent->subtrees_count * sizeof(tree_item));
+}
+
+void prefix_traverse(tree target_tree, void (*callback)(T value, int depth))
+{
+    prefix_traverse_inner(target_tree.root, 0, callback);
+}
+
+void prefix_traverse_inner(tree_item* subtree_root, int depth, void (*callback)(T value, int depth))
+{
+    int i;
+
+    if (subtree_root == NULL)
+    {
+        return;
+    }
+
+    callback(subtree_root->data, depth);
+
+    for (i = 0; i < subtree_root->subtrees_count; i++)
+    {
+        prefix_traverse_inner(subtree_root->subtrees + i, depth + 1, callback);
     }
 }
 
-void tree_remove(Tree *tree)
+bool is_structured_like_linear_list(tree target_tree)
 {
-    if (((*tree) != NULL) && ((*tree)->children != NULL)) {
-        Forest child = (*tree)->first_child;
+    tree_item* iterator;
 
-        while (child != NULL) {
-            free(child->prev);
-            tree_remove(&(child->node));
-            child = child->next;
-        }
-
-        free((*tree)->children);
+    if ((iterator = target_tree.root) == NULL)
+    {
+        // TODO: ?!
+        return no;
     }
 
-    free(*tree);
-    *tree = NULL;
+    while (1)
+    {
+        if (iterator->subtrees_count > 1)
+        {
+            return no;
+        }
+        else if (iterator->subtrees_count == 0)
+        {
+            return yes;
+        }
+        iterator = iterator->subtrees;
+    }
 }
 
-Tree *tree_find(Tree *tree, TreeItem c)
+void clear(tree* target_tree)
 {
-    if ((*tree) == NULL) {
-        return NULL;
+    if (target_tree == NULL)
+    {
+        return;
     }
 
-    if ((*tree)->value == c) {
-        return tree;
-    }
-
-    Tree *result = NULL;
-
-    if ((*tree)->children != NULL) {
-        for (Forest child = (*tree)->first_child; result == NULL && child != NULL;
-             child = child->next) {
-            result = tree_find(&(child->node), c);
-        }
-    }
-
-    return result;
-}
-
-bool is_linear_list(Tree tree)
-{
-    if (tree == NULL) {
-        return false;
-    } else if (tree->children == NULL) {
-        return true;
-    }
-
-    bool is_internal = true;
-
-    for (Forest child = tree->first_child; child != NULL; child = child->next) {
-        if (child->next != NULL && child->next->node != NULL) {
-            return false;
-        }
-    }
-    return is_internal;
+    remove_item(target_tree, target_tree->root);
 }
